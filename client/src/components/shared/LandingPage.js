9661d5349fbf5975a3ec51a5b56bc0fb9063eb9d
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-05-17 13:28:39 -0600

    added landing page

---
+++ client/src/components/shared/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LandingPage  = () => {
+  return (
+    <div>
+      <h1>Welcome! </h1> 
+      <h4>TODO: This page prompts user to sign-in or sign-up</h4>
+
+    </div>
+  )
+}
+
+export default LandingPage;
