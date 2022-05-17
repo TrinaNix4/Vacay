53440086a3f4888cc43898f6d64828d419e56159
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-05-17 13:26:31 -0600

    began the edit profile page

---
+++ client/src/components/auth/EditProfile.js
@@ -0,0 +1,30 @@
+import React, {useState, useContext } from 'react'; 
+import { AuthContext } from '../../providers/AuthProvider';
+
+const EditProfile = () => {
+  // const [user, setUser] = useContext(AuthContext);
+  // const [nickname, setNickname] = useState(user.username); 
+  // const [password, setPassword] = useState(user.password);
+  // const [name, setName] = useState(user.name);
+  // const [email, setEmail] = useState(user.email);
+  // const [phone, setPhone] = useState(user.phone);
+
+  return (
+    <div>
+      {/* <form onSubmit={handleSubmit}>
+      <button onClick= {() => }>
+
+      </button>
+    </form>
+      <input>
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        
+      </input> */}
+
+      <h1> Edit Profile </h1>
+    </div>
+  );
+
+    }
+export default EditProfile; 
