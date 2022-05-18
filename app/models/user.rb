8396a080096641958e61ad8f0986971f893c20c3
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-05-18 10:25:20 -0600

    added user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+    belongs_to :group
     extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
