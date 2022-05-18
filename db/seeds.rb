d4088e7d5cd9ec2ae2056a84f3707d6d8dbd9a49
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-05-18 10:26:14 -0600

    added group data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,16 @@ u1 = User.create(email:'thecrow@gmail.com', password:123456, name:'Eric Draven',
 u2 = User.create(email:'ilovehalloween@test.com', password:123456, name:'Michael Myers', nickname:'cold_blooded_killa', image:'https://pbs.twimg.com/profile_images/1466466892223234053/ihMBEiVB_400x400.jpg', phone:'801777777')
 u3 = User.create(email:'ripley@test.com', password:123456, name:'Ellen Ripley', nickname:'alien_slayer', image:'https://i.guim.co.uk/img/media/ef051ec5cc2f1d6c0ff5f18ee7225b04e54fa18e/481_491_2408_1445/master/2408.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=68022631661a93655ae2d1066fcf3c26', phone:'801777777')
 u4 = User.create(email:'sherlocked@test.com', password:123456, name:'Sherlock Holmes', nickname:'sherlocked', image:'https://media.npr.org/assets/img/2018/06/02/sherlock-1-e20e4d122bb80306472eaf22e07a19969d6fc202-s1100-c50.jpg', phone:'801777777')
-u5 = User.create(email:'007@test.com', password:123456, name:'James Bond', nickname:'007', image:'https://sdl-stickershop.line.naver.jp/products/0/0/1/1361417/android/stickers/14287238.png;compress=true', phone:'801777777')
+u5 = User.create(email:'007@test.com', password:123456, name:'James Bond', nickname:'007', image:'https://sdl-stickershop.line.naver.jp/products/0/0/1/1361417/android/stickers/14287238.png;compress=true', phone:'801777777') 
+
+
+u1.groups.create(name: 'group1', image:'', location:'pool')
+u1.groups.create(name: 'group2', image:'', location:'bar')
+u2.groups.create(name: 'group1', image:'', location:'pool')
+u2.groups.create(name: 'group2', image:'', location:'bar')
+u3.groups.create(name: 'group1', image:'', location:'pool')
+u3.groups.create(name: 'group3', image:'', location:'room 343')
+u4.groups.create(name: 'group2', image:'', location:'bar')
+u4.groups.create(name: 'group3', image:'', location:'room 343')
+u5.groups.create(name: 'group2', image:'', location:'bar')
+u5.groups.create(name: 'group4', image:'', location:'room 138')
