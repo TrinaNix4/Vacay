e4e2c0c9d4288451b88db2265cee7e02f84cda54
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-05-20 10:06:14 -0600

    basic crud on users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,41 @@
 class Api::UsersController < ApplicationController
+  before_action :set_group
+  before_action :set_user, only:[:show, :udpate, :destroy]
+
+  #get all users of a specific group
+  #get 'api/groups/:group_id/users'
+  # axios.get('/api/groups/1/users')
+  def index 
+    render json: @group.users
+  end 
+
+  #get
+  def show 
+    render json: @user
+  end
+
+ 
+  def update
+    if(current_user.update(user_params))
+        render json: current_user
+    else
+        render json: {errors: current_user.errors.full_messages}, status: 422
+    end
+  end
+
+ 
+  
+  private 
+def set_group 
+  @group = Group.find(params[:group_id])
+end
+
+def set_user
+  @user = @group.users.find(params[:id])
+end 
+
+
+  def user_params
+    params.require(:user).permit(:name, :nickname, :phone, :email, :password, :image, :group_id)
+  end
 end
